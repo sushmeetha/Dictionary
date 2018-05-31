c041a6fb1864a8d2c4f4f7eb72d5c308eaa0f6a9
Sushmeetha <ssushmeetha@gmail.com>
2018-05-31 13:50:38 +1000

    Implementing Dictiong Sorting and Counting words with java and DS Algorithm
    Implementing Dictiong Sorting and Counting words with java and DS Algorithm

---
+++ Dictionary/src/dictionary/SortedDictionaryCounting.java
@@ -0,0 +1,13 @@
+package dictionary;
+	public class SortedDictionaryCounting
+	{
+		static String Text = "This is a sample program " +
+
+			"This is a sample program and I have entered " +
+
+			"This is a sample program and I have entered this text " +
+
+			"This is a sample program and I have entered this text to check it ";
+
+	}
+	
---
+++ Dictionary/src/dictionary/StringHelper.java
@@ -0,0 +1,14 @@
+package dictionary;
+
+public class StringHelper {
+
+	public static boolean isNullOrEmpty(String trim) {
+		String trim1=trim;
+		if( trim1!=null || trim1.isEmpty()) 	
+		
+			return false;
+		else
+			return true;
+	}
+
+}
